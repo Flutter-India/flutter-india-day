bec0c5a0ecd9c9956e443c926b35cbe388fa0824
Yash Adulkar <yash151099@gmail.com>
2021-04-28 16:26:28 +0530

    Update flutter_service_worker.js

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "6b083db73c7f83adc4d11063ee536728",
-"index.html": "eb476d15742e1ab648cb3e64976d1819",
-"/": "eb476d15742e1ab648cb3e64976d1819",
-"main.dart.js": "b78ec372e7a38d1357faee01e4cda11e",
+"index.html": "2c0a377a81885b70cbc9395738258ee3",
+"/": "2c0a377a81885b70cbc9395738258ee3",
+"main.dart.js": "14491578aadfbbf1d60eea8137506b62",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
